6fa2e706fdf2d2183db726326cbbf40e8b66a585
Sergei <162130708+ArdSkellige@users.noreply.github.com>
2025-04-04 10:40:03 +0300

    Delete build directory

--- build/qt_liba_autogen/ZSIHZCJSBK/moc_rectangle.cpp.d
+++
@@ -1,2 +0,0 @@
-C:/projects/cmake_exercise/build/qt_liba_autogen/ZSIHZCJSBK/moc_rectangle.cpp: C:/projects/cmake_exercise/qt_app/rectangle.hpp \
-  C:/projects/cmake_exercise/build/qt_liba_autogen/moc_predefs.h
